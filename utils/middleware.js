6ebfaf46f3fb0b32838b082b3ac68105ded85603
liangdong.xu <liangdong.xu@leyantech.com>
2021-04-08 16:44:12 +0800

    setHeader version

--- utils/middleware.js
+++ utils/middleware.js
@@ -1,6 +1,7 @@
 const logger = require('./logger')
 
 const requestLogger = (request, response, next) => {
+  response.setHeader('version', process.env.VERSION)
   logger.info('Method:', request.method)
   logger.info('Path:  ', request.path)
   logger.info('Body:  ', request.body)
