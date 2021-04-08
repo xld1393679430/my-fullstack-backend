6593ee7b5e1b229f887ab3086a27b1fd174994a8
liangdong.xu <liangdong.xu@leyantech.com>
2021-04-08 20:18:33 +0800

    expired time

--- controllers/login.js
+++ controllers/login.js
@@ -21,7 +21,12 @@ loginRouter.post('/', async (req, res) => {
     id: user._id,
   }
 
-  const token = jwt.sign(userForToken, process.env.SECRET)
+  // expired time: 12h
+  const token = jwt.sign(
+    userForToken,
+    process.env.SECRET,
+    { expiresIn: 12 * 60 * 60 }
+  )
 
   res.status(200).send({
     token,
